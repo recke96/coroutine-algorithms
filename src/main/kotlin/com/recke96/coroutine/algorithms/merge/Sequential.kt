7fd1f2970a8151c4d957767322ec13cf272ece25
recke96 <jakob.ecker@gmx.at>
2019-05-08 12:34:10 +0200

    remove unnessecary val

--- src/main/kotlin/com/recke96/coroutine/algorithms/merge/Sequential.kt
+++ src/main/kotlin/com/recke96/coroutine/algorithms/merge/Sequential.kt
@@ -1,8 +1,7 @@
 package com.recke96.coroutine.algorithms.merge
 
 fun <T: Comparable<T>> mergeSeq(a: List<T>, b: List<T>): List<T> {
-    val elements = a.size + b.size
-    val merged = ArrayList<T>(elements)
+    val merged = ArrayList<T>(a.size + b.size)
     var i = 0; var j = 0
 
     while (i < a.size && j < b.size){
